977f2b0ad6494d25177714402738206be4555846
neocleous <47337157+neocleous@users.noreply.github.com>
2026-03-15 20:57:25 +0100

    Update bme680.py

--- sensor-daemon/sensors/bme680.py
+++ sensor-daemon/sensors/bme680.py
@@ -1,21 +1,27 @@
-import adafruit_bme680
-import busio
-import board
-import digitalio
+import bme680
+import smbus2
 
 class BME680:
     def __init__(self):
-        i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
-        self.sensor.sea_level_pressure = 1013.25
+        self.sensor = bme680.BME680(i2c_addr=0x76, i2c_device=smbus2.SMBus(1))
+        self.sensor.set_humidity_oversample(bme680.OS_2X)
+        self.sensor.set_pressure_oversample(bme680.OS_4X)
+        self.sensor.set_temperature_oversample(bme680.OS_8X)
+        self.sensor.set_filter(bme680.FILTER_SIZE_3)
+        self.sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+        self.sensor.set_gas_heater_temperature(320)
+        self.sensor.set_gas_heater_duration(150)
+        self.sensor.select_gas_heater_profile(0)
 
     def read(self):
         try:
-            return {
-                "temperature": round(self.sensor.temperature, 1),
-                "humidity": round(self.sensor.relative_humidity, 1),
-                "pressure": round(self.sensor.pressure, 1),
-                "voc": round(self.sensor.gas, 0)
-            }
+            if self.sensor.get_sensor_data():
+                return {
+                    "temperature": round(self.sensor.data.temperature, 1),
+                    "humidity": round(self.sensor.data.humidity, 1),
+                    "pressure": round(self.sensor.data.pressure, 1),
+                    "voc": round(self.sensor.data.gas_resistance, 0)
+                }
+            return None
         except Exception as e:
             return None
