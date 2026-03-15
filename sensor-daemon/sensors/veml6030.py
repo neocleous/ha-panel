7cc4d724f0e4b5f84ce892070f5189d13964266c
neocleous <47337157+neocleous@users.noreply.github.com>
2026-03-15 22:34:24 +0100

    Update veml6030.py

--- sensor-daemon/sensors/veml6030.py
+++ sensor-daemon/sensors/veml6030.py
@@ -1,14 +1,20 @@
 import adafruit_veml7700
 import busio
 import board
-import digitalio
 
 class VEML6030:
     def __init__(self):
-        i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_veml7700.VEML7700(i2c)
+        try:
+            i2c = busio.I2C(board.SCL, board.SDA)
+            self.sensor = adafruit_veml7700.VEML7700(i2c)
+            self._available = True
+        except Exception as e:
+            self._available = False
+            self.sensor = None
 
     def read(self):
+        if not self._available:
+            return None
         try:
             return round(self.sensor.lux, 1)
         except Exception as e:
