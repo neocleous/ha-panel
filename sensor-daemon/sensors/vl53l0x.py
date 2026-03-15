206ef7b0265ed076847803d05703fe613e6b93e6
neocleous <47337157+neocleous@users.noreply.github.com>
2026-03-15 22:33:57 +0100

    Update vl53l0x.py

--- sensor-daemon/sensors/vl53l0x.py
+++ sensor-daemon/sensors/vl53l0x.py
@@ -1,14 +1,20 @@
 import adafruit_vl53l0x
 import busio
 import board
-import digitalio
 
 class VL53L0X:
     def __init__(self):
-        i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_vl53l0x.VL53L0X(i2c)
+        try:
+            i2c = busio.I2C(board.SCL, board.SDA)
+            self.sensor = adafruit_vl53l0x.VL53L0X(i2c)
+            self._available = True
+        except Exception as e:
+            self._available = False
+            self.sensor = None
 
     def read(self):
+        if not self._available:
+            return None
         try:
             distance = self.sensor.range
             if distance >= 8190:
